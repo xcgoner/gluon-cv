11479f06e4c514294e62a7faf83a74c782f4402f
unknown <xcgoner1108@gmail.com>
2019-12-26 20:59:05 -0600

    bash

--- scripts/classification/cifar/exp_04.sh
+++ scripts/classification/cifar/exp_04.sh
@@ -14,3 +14,17 @@ do
         done
     done
 done
+
+for i in 1 2 3 4 5
+do
+    for lr in 0.0005 0.001
+    do
+        for batchsize in 8 16 32
+        do 
+            horovodrun -np 8 -H localhost:8 python3 train_cifar100_hvd_compr.py --model cifar_wideresnet40_8 \
+            --optimizer signum --lr ${lr} --lr-decay 0.1 --lr-decay-epoch 60,120 --wd 0.0005 --reset-interval 0 \
+            --sparse-ratio 0 --num-epochs 150 --batch-size ${batchsize}
+            sleep 60
+        done
+    done
+done
