527aadff7ffb483980227c0bd655cb7ef363f6b2
unknown <xcgoner1108@gmail.com>
2020-04-07 18:20:53 -0500

    ersgd experiments

--- scripts/classification/cifar/exp_ersgd.sh
+++ scripts/classification/cifar/exp_ersgd.sh
@@ -1,6 +1,6 @@
-for rowsparse in 32 64
+for rowsparse in 32 
 do
-    for layersparse in 1 2 4
+    for layersparse in 1 2 4 5
     do
         for lr in 0.01 0.05 0.1
         do
@@ -9,7 +9,7 @@ do
                 horovodrun -np 4 -H localhost:4 python3 train_cifar100_hvd_ersgd_v2.py \
                 --model cifar_wideresnet40_8 --optimizer ersgdv2 --lr ${lr} --lr-decay 0.2 --nesterov \
                 --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size ${batchsize} \
-                --row-sparse ${rowsparse} --layer-sparse ${layersparse} 
+                --input-sparse ${rowsparse} --output-sparse ${rowsparse} --layer-sparse ${layersparse} 
                 sleep 60
             done
         done
