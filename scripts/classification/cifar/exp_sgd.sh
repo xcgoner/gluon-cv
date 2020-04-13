e0d91ea117c2239d29df443f1c508cde28d8e65b
unknown <xcgoner1108@gmail.com>
2020-04-13 14:38:57 -0500

    scripts

--- scripts/classification/cifar/exp_sgd.sh
+++ scripts/classification/cifar/exp_sgd.sh
@@ -1,15 +1,17 @@
+#!/bin/sh
+HOSTFILE=$1
 for repeat in 1 2 3 4 5
 do
     for lr in 0.1
     do
         for batchsize in 16
         do
-            horovodrun -np 8 -hostfile ~/hosts/xcong-imagenet python3 train_cifar100_hvd.py \
+            horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_cifar100_hvd.py \
             --model cifar_wideresnet40_8 --optimizer nag --lr ${lr} --lr-decay 0.2 \
             --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size ${batchsize}
 
             sleep 60
-            bash ~/src/ersgd/pkill_cluster.sh ~/hosts/xcong-imagenet-0
+            bash ~/src/ersgd/pkill_cluster.sh ${HOSTFILE}
             sleep 60
         done
     done
