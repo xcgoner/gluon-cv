04bf4069720ca96bdc1a84badc792043069de5ae
unknown <xcgoner1108@gmail.com>
2020-04-12 23:44:53 -0500

    version2

--- scripts/classification/cifar/exp_sgd.sh
+++ scripts/classification/cifar/exp_sgd.sh
@@ -1,20 +1,16 @@
-for lr in 0.01 0.05 0.1
+for repeat in 1 2 3 4 5
 do
-    for batchsize in 32
+    for lr in 0.1
     do
-        horovodrun -np 4 -H localhost:4 python3 train_cifar100_hvd.py \
-        --model cifar_wideresnet40_8 --optimizer nag --lr ${lr} --lr-decay 0.2 \
-        --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size ${batchsize}
-        sleep 60
-    done
-done
-for lr in 0.01 0.05 0.1
-do
-    for batchsize in 32
-    do
-        horovodrun -np 4 -H localhost:4 python3 train_cifar100_hvd.py \
-        --model cifar_wideresnet40_8 --optimizer nag --lr ${lr} --lr-decay 0.2 \
-        --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size ${batchsize}
-        sleep 60
+        for batchsize in 16
+        do
+            horovodrun -np 8 -hostfile ~/hosts/xcong-imagenet python3 train_cifar100_hvd.py \
+            --model cifar_wideresnet40_8 --optimizer nag --lr ${lr} --lr-decay 0.2 \
+            --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size ${batchsize}
+
+            sleep 60
+            bash ~/src/ersgd/pkill_cluster.sh ~/hosts/xcong-imagenet-0
+            sleep 60
+        done
     done
 done
