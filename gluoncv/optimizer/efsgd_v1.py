aa41e75aa61b73eea92ec4a05ae882c39ee58e89
unknown <xcgoner1108@gmail.com>
2020-04-17 23:04:47 -0500

    imagenet hvd

--- gluoncv/optimizer/efsgd_v1.py
+++ gluoncv/optimizer/efsgd_v1.py
@@ -21,7 +21,7 @@ import warnings
 import numpy
 from mxnet.optimizer import Optimizer, register
 from mxnet.ndarray import zeros, NDArray, full
-from mxnet.ndarray import efsgd_pre_update
+from mxnet.ndarray import efsgd_pre_update, mp_efsgd_pre_update
 
 __all__ = ['EFSGDV1']
 
@@ -35,14 +35,28 @@ class EFSGDV1(Optimizer):
         self.momentum = momentum
         self.nesterov = nesterov
 
+    def create_state_multi_precision(self, index, weight):
+        weight_master_copy = None
+        if self.multi_precision and weight.dtype == numpy.float16:
+            weight_master_copy = weight.astype(numpy.float32)
+            return (zeros(weight.shape, weight.context, dtype=weight.dtype), # e, remaining error
+                    zeros(weight.shape, weight.context, dtype=weight.dtype), # m, momentum
+                    zeros(weight.shape, weight.context, dtype=weight.dtype), # m_wd, momentum of weight decay
+                    weight_master_copy) # the float32 copy of weight
+        if weight.dtype == numpy.float16 and not self.multi_precision:
+            warnings.warn("Accumulating with float16 in optimizer can lead to "
+                          "poor accuracy or slow convergence. "
+                          "Consider using multi_precision=True option of the "
+                          "NAG optimizer")
+        return self.create_state(index, weight)
+
     def create_state(self, _, weight):
         """state creation function."""
-        return (zeros(weight.shape, weight.context, dtype=weight.dtype), #e, remaining error
-                zeros(weight.shape, weight.context, dtype=weight.dtype), #m, momentum
-                zeros(weight.shape, weight.context, dtype=weight.dtype)) #m_wd, momentum of weight decay
+        return (zeros(weight.shape, weight.context, dtype=weight.dtype), # e, remaining error
+                zeros(weight.shape, weight.context, dtype=weight.dtype), # m, momentum
+                zeros(weight.shape, weight.context, dtype=weight.dtype)) # m_wd, momentum of weight decay
 
-    def update(self, index, weight, grad, state):
-        """update function"""
+    def _update_impl(self, index, weight, grad, state, multi_precision=False):
         assert(isinstance(weight, NDArray))
         assert(isinstance(grad, NDArray))
         self._update_count(index)
@@ -54,6 +68,24 @@ class EFSGDV1(Optimizer):
         if self.clip_gradient:
             kwargs['clip_gradient'] = self.clip_gradient
 
-        e, m, m_wd = state
-        efsgd_pre_update(weight, grad, e, m, m_wd, out=weight,
-                    lr=lr, wd=wd, **kwargs)
+        if not multi_precision:
+            e, m, m_wd = state
+            efsgd_pre_update(weight, grad, e, m, m_wd, out=weight,
+                        lr=lr, wd=wd, **kwargs)
+        else:
+            e, m, m_wd, w_32 = state
+            mp_efsgd_pre_update(weight, grad, e, m, m_wd, w_32, out=weight,
+                        lr=lr, wd=wd, **kwargs)
+
+    def update(self, index, weight, grad, state):
+        self._update_impl(index, weight, grad, state, multi_precision=False)
+
+    def update_multi_precision(self, index, weight, grad, state):
+        use_multi_precision = self.multi_precision and weight.dtype == numpy.float16 \
+                                and isinstance(state, (tuple, list))
+        self._update_impl(index, weight, grad, state,
+                          multi_precision=use_multi_precision)
+
+        
+
+        
