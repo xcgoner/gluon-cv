abf8ca3fca0e304fbe5719435baf5b57719323b3
unknown <xcgoner1108@gmail.com>
2020-04-26 02:20:19 -0500

    warmup

--- scripts/classification/cifar/exp_ersgd2_128.sh
+++ scripts/classification/cifar/exp_ersgd2_128.sh
@@ -6,7 +6,7 @@ for repeat in 1; do
     outputsparse1=8
     layersparse1=1
     inputsparse2=1
-    for warmup in 0.5 1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0; do
+    for warmup in 2.0 3.0 4.0 5.0 6.0 7.0 8.0; do
         for lr in 0.05 0.1; do
             for batchsize in 16; do
                 let localsgdinterval=inputsparse1*outputsparse1*layersparse1/inputsparse2
