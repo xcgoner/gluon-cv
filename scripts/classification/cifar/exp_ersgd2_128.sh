4f3552373938e695c51a74be596548173a5785ed
unknown <xcgoner1108@gmail.com>
2020-04-28 01:08:13 -0500

    warmup

--- scripts/classification/cifar/exp_ersgd2_128.sh
+++ scripts/classification/cifar/exp_ersgd2_128.sh
@@ -5,14 +5,13 @@ for repeat in 1; do
     inputsparse1=32
     outputsparse1=8
     layersparse1=1
-    inputsparse2=4
-    for warmup in 3.0 4.0 2.0; do
+    for warmup in 3.0 2.0 1.0; do
         for lr in 0.1; do
-            for batchsize in 16; do
+            for inputsparse2 in 4 2 1; do
                 let localsgdinterval=inputsparse1*outputsparse1*layersparse1/inputsparse2
                 horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_cifar100_hvd_ersgd2_v2.py \
                 --model cifar_wideresnet40_8 --optimizer nag --lr ${lr} --lr-decay 0.2 \
-                --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size ${batchsize} \
+                --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size 16 \
                 --input-sparse-1 ${inputsparse1} --output-sparse-1 ${outputsparse1} --layer-sparse-1 ${layersparse1} \
                 --input-sparse-2 ${inputsparse2} --output-sparse-2 1 --layer-sparse-2 1 \
                 --local-sgd-interval ${localsgdinterval} --warmup ${warmup}
