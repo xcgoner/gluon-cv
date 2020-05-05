25bc37c5991b0264fe20da3d82199ab88a903d1e
unknown <xcgoner1108@gmail.com>
2020-05-05 14:15:38 -0500

    warmup

--- scripts/classification/cifar/exp_ersgd2.sh
+++ scripts/classification/cifar/exp_ersgd2.sh
@@ -1,83 +1,84 @@
 #!/bin/sh
 HOSTFILE=$1
 
-for repeat in 1; do
-    for inputsparse1 in 4 8 16 32; do
-        for outputsparse1 in 1; do
-            for inputsparse2 in 2; do
-                for lr in 0.1; do
-                    for batchsize in 16; do
-                        let localsgdinterval=inputsparse1*outputsparse1/inputsparse2
-                        horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_cifar100_hvd_ersgd2_v2.py \
-                        --model cifar_wideresnet40_8 --optimizer nag --lr ${lr} --lr-decay 0.2 \
-                        --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size ${batchsize} \
-                        --input-sparse-1 ${inputsparse1} --output-sparse-1 ${outputsparse1} --layer-sparse-1 1 \
-                        --input-sparse-2 ${inputsparse2} --output-sparse-2 1 --layer-sparse-2 1 \
-                        --local-sgd-interval ${localsgdinterval} --warmup 2.0
+# for repeat in 1; do
+#     for inputsparse1 in 4 8 16 32; do
+#         for outputsparse1 in 1; do
+#             for inputsparse2 in 2; do
+#                 for lr in 0.1; do
+#                     for batchsize in 16; do
+#                         let localsgdinterval=inputsparse1*outputsparse1/inputsparse2
+#                         horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_cifar100_hvd_ersgd2_v2.py \
+#                         --model cifar_wideresnet40_8 --optimizer nag --lr ${lr} --lr-decay 0.2 \
+#                         --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size ${batchsize} \
+#                         --input-sparse-1 ${inputsparse1} --output-sparse-1 ${outputsparse1} --layer-sparse-1 1 \
+#                         --input-sparse-2 ${inputsparse2} --output-sparse-2 1 --layer-sparse-2 1 \
+#                         --local-sgd-interval ${localsgdinterval} --warmup 2.0
 
-                        sleep 60
-                        bash /home/ubuntu/src/ersgd/ec2-tools/pkill_cluster.sh ${HOSTFILE}-0
-                        sleep 60
-                    done
-                done
-            done
-        done
-    done
-done
+#                         sleep 60
+#                         bash /home/ubuntu/src/ersgd/ec2-tools/pkill_cluster.sh ${HOSTFILE}-0
+#                         sleep 60
+#                     done
+#                 done
+#             done
+#         done
+#     done
+# done
 
-for repeat in 1; do
-    for inputsparse1 in 8 16 32; do
-        for outputsparse1 in 1; do
-            for inputsparse2 in 4; do
-                for lr in 0.1; do
-                    for batchsize in 16; do
-                        let localsgdinterval=inputsparse1*outputsparse1/inputsparse2
-                        horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_cifar100_hvd_ersgd2_v2.py \
-                        --model cifar_wideresnet40_8 --optimizer nag --lr ${lr} --lr-decay 0.2 \
-                        --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size ${batchsize} \
-                        --input-sparse-1 ${inputsparse1} --output-sparse-1 ${outputsparse1} --layer-sparse-1 1 \
-                        --input-sparse-2 ${inputsparse2} --output-sparse-2 1 --layer-sparse-2 1 \
-                        --local-sgd-interval ${localsgdinterval} --warmup 2.0
+# for repeat in 1; do
+#     for inputsparse1 in 8 16 32; do
+#         for outputsparse1 in 1; do
+#             for inputsparse2 in 4; do
+#                 for lr in 0.1; do
+#                     for batchsize in 16; do
+#                         let localsgdinterval=inputsparse1*outputsparse1/inputsparse2
+#                         horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_cifar100_hvd_ersgd2_v2.py \
+#                         --model cifar_wideresnet40_8 --optimizer nag --lr ${lr} --lr-decay 0.2 \
+#                         --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size ${batchsize} \
+#                         --input-sparse-1 ${inputsparse1} --output-sparse-1 ${outputsparse1} --layer-sparse-1 1 \
+#                         --input-sparse-2 ${inputsparse2} --output-sparse-2 1 --layer-sparse-2 1 \
+#                         --local-sgd-interval ${localsgdinterval} --warmup 2.0
 
-                        sleep 60
-                        bash /home/ubuntu/src/ersgd/ec2-tools/pkill_cluster.sh ${HOSTFILE}-0
-                        sleep 60
-                    done
-                done
-            done
-        done
-    done
-done
+#                         sleep 60
+#                         bash /home/ubuntu/src/ersgd/ec2-tools/pkill_cluster.sh ${HOSTFILE}-0
+#                         sleep 60
+#                     done
+#                 done
+#             done
+#         done
+#     done
+# done
 
-for repeat in 1; do
-    for inputsparse1 in 16 32; do
-        for outputsparse1 in 1; do
-            for inputsparse2 in 8; do
-                for lr in 0.1; do
-                    for batchsize in 16; do
-                        let localsgdinterval=inputsparse1*outputsparse1/inputsparse2
-                        horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_cifar100_hvd_ersgd2_v2.py \
-                        --model cifar_wideresnet40_8 --optimizer nag --lr ${lr} --lr-decay 0.2 \
-                        --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size ${batchsize} \
-                        --input-sparse-1 ${inputsparse1} --output-sparse-1 ${outputsparse1} --layer-sparse-1 1 \
-                        --input-sparse-2 ${inputsparse2} --output-sparse-2 1 --layer-sparse-2 1 \
-                        --local-sgd-interval ${localsgdinterval} --warmup 1.0
+# for repeat in 1; do
+#     for inputsparse1 in 16 32; do
+#         for outputsparse1 in 1; do
+#             for inputsparse2 in 8; do
+#                 for lr in 0.1; do
+#                     for batchsize in 16; do
+#                         let localsgdinterval=inputsparse1*outputsparse1/inputsparse2
+#                         horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_cifar100_hvd_ersgd2_v2.py \
+#                         --model cifar_wideresnet40_8 --optimizer nag --lr ${lr} --lr-decay 0.2 \
+#                         --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size ${batchsize} \
+#                         --input-sparse-1 ${inputsparse1} --output-sparse-1 ${outputsparse1} --layer-sparse-1 1 \
+#                         --input-sparse-2 ${inputsparse2} --output-sparse-2 1 --layer-sparse-2 1 \
+#                         --local-sgd-interval ${localsgdinterval} --warmup 1.0
 
-                        sleep 60
-                        bash /home/ubuntu/src/ersgd/ec2-tools/pkill_cluster.sh ${HOSTFILE}-0
-                        sleep 60
-                    done
-                done
-            done
-        done
-    done
-done
+#                         sleep 60
+#                         bash /home/ubuntu/src/ersgd/ec2-tools/pkill_cluster.sh ${HOSTFILE}-0
+#                         sleep 60
+#                     done
+#                 done
+#             done
+#         done
+#     done
+# done
 
 # aggressive
 
-for repeat in 5; do
+for repeat in 1 2 3 4; do
+
     for inputsparse1 in 32; do
-        for outputsparse1 in 2 4 8 16 32; do
+        for outputsparse1 in 2 4 8 16; do
             for inputsparse2 in 1 4 8 16; do
                 for lr in 0.1; do
                     for batchsize in 16; do
@@ -97,22 +98,19 @@ for repeat in 5; do
             done
         done
     done
-done
 
-for repeat in 5; do
-    layersparse1=2
     for inputsparse1 in 32; do
         for outputsparse1 in 32; do
-            for inputsparse2 in 1 4 8 16 32; do
-                for lr in 0.1; do
+            for inputsparse2 in 1 4 8 16; do
+                for lr in 0.05; do
                     for batchsize in 16; do
-                        let localsgdinterval=inputsparse1*outputsparse1*layersparse1/inputsparse2
+                        let localsgdinterval=inputsparse1*outputsparse1/inputsparse2
                         horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_cifar100_hvd_ersgd2_v2.py \
                         --model cifar_wideresnet40_8 --optimizer nag --lr ${lr} --lr-decay 0.2 \
                         --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size ${batchsize} \
-                        --input-sparse-1 ${inputsparse1} --output-sparse-1 ${outputsparse1} --layer-sparse-1 ${layersparse1} \
+                        --input-sparse-1 ${inputsparse1} --output-sparse-1 ${outputsparse1} --layer-sparse-1 1 \
                         --input-sparse-2 ${inputsparse2} --output-sparse-2 1 --layer-sparse-2 1 \
-                        --local-sgd-interval ${localsgdinterval} --warmup 3.0 --sync-states
+                        --local-sgd-interval ${localsgdinterval} --warmup 2.0 --sync-states
 
                         sleep 60
                         bash /home/ubuntu/src/ersgd/ec2-tools/pkill_cluster.sh ${HOSTFILE}-0
@@ -122,6 +120,30 @@ for repeat in 5; do
             done
         done
     done
+
+    layersparse1=2
+    for inputsparse1 in 32; do
+        for outputsparse1 in 32; do
+            for inputsparse2 in 1 4 8 16 32; do
+                for lr in 0.05; do
+                    for batchsize in 16; do
+                        let localsgdinterval=inputsparse1*outputsparse1*layersparse1/inputsparse2
+                        horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_cifar100_hvd_ersgd2_v2.py \
+                        --model cifar_wideresnet40_8 --optimizer nag --lr ${lr} --lr-decay 0.2 \
+                        --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size ${batchsize} \
+                        --input-sparse-1 ${inputsparse1} --output-sparse-1 ${outputsparse1} --layer-sparse-1 ${layersparse1} \
+                        --input-sparse-2 ${inputsparse2} --output-sparse-2 1 --layer-sparse-2 1 \
+                        --local-sgd-interval ${localsgdinterval} --warmup 2.0 --sync-states
+
+                        sleep 60
+                        bash /home/ubuntu/src/ersgd/ec2-tools/pkill_cluster.sh ${HOSTFILE}-0
+                        sleep 60
+                    done
+                done
+            done
+        done
+    done
+
 done
 
 
