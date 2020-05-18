756145e0dedb0947c50a910968532865e67804d8
unknown <xcgoner1108@gmail.com>
2020-05-18 12:03:36 -0500

    imagenet

--- scripts/classification/imagenet/exp_imagenet.sh
+++ scripts/classification/imagenet/exp_imagenet.sh
@@ -53,40 +53,40 @@ do
     # bash /home/ubuntu/src/ersgd/ec2-tools/pkill_cluster.sh ${HOSTFILE}-0
     # sleep 60
 
-    horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_imagenet_hvd.py   \
-    --rec-train /home/ubuntu/data/imagenet/train.rec --rec-train-idx /home/ubuntu/data/imagenet/train.idx   \
-    --rec-val /home/ubuntu/data/imagenet/val.rec --rec-val-idx /home/ubuntu/data/imagenet/val.idx   \
-    --model resnet50_v2 --mode hybrid   --lr 0.05 --lr-mode cosine --num-epochs 120 --batch-size 32 -j 6   \
-    --warmup-epochs 5 --use-rec --dtype float16 --optimizer nag \
-    --trainer efsgd --input-sparse-1 1 --output-sparse-1 1 --layer-sparse-1 32
+    # horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_imagenet_hvd.py   \
+    # --rec-train /home/ubuntu/data/imagenet/train.rec --rec-train-idx /home/ubuntu/data/imagenet/train.idx   \
+    # --rec-val /home/ubuntu/data/imagenet/val.rec --rec-val-idx /home/ubuntu/data/imagenet/val.idx   \
+    # --model resnet50_v2 --mode hybrid   --lr 0.05 --lr-mode cosine --num-epochs 120 --batch-size 32 -j 6   \
+    # --warmup-epochs 5 --use-rec --dtype float16 --optimizer nag \
+    # --trainer efsgd --input-sparse-1 1 --output-sparse-1 1 --layer-sparse-1 32
 
-    sleep 60
-    bash /home/ubuntu/src/ersgd/ec2-tools/pkill_cluster.sh ${HOSTFILE}-0
-    sleep 60
+    # sleep 60
+    # bash /home/ubuntu/src/ersgd/ec2-tools/pkill_cluster.sh ${HOSTFILE}-0
+    # sleep 60
 
 
-    horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_imagenet_hvd.py   \
-    --rec-train /home/ubuntu/data/imagenet/train.rec --rec-train-idx /home/ubuntu/data/imagenet/train.idx   \
-    --rec-val /home/ubuntu/data/imagenet/val.rec --rec-val-idx /home/ubuntu/data/imagenet/val.idx   \
-    --model resnet50_v2 --mode hybrid   --lr 0.05 --lr-mode cosine --num-epochs 120 --batch-size 32 -j 6   \
-    --warmup-epochs 5 --use-rec --dtype float16 --optimizer nag \
-    --trainer efsgd --input-sparse-1 1 --output-sparse-1 1 --layer-sparse-1 256
+    # horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_imagenet_hvd.py   \
+    # --rec-train /home/ubuntu/data/imagenet/train.rec --rec-train-idx /home/ubuntu/data/imagenet/train.idx   \
+    # --rec-val /home/ubuntu/data/imagenet/val.rec --rec-val-idx /home/ubuntu/data/imagenet/val.idx   \
+    # --model resnet50_v2 --mode hybrid   --lr 0.05 --lr-mode cosine --num-epochs 120 --batch-size 32 -j 6   \
+    # --warmup-epochs 5 --use-rec --dtype float16 --optimizer nag \
+    # --trainer efsgd --input-sparse-1 1 --output-sparse-1 1 --layer-sparse-1 256
 
-    sleep 60
-    bash /home/ubuntu/src/ersgd/ec2-tools/pkill_cluster.sh ${HOSTFILE}-0
-    sleep 60
+    # sleep 60
+    # bash /home/ubuntu/src/ersgd/ec2-tools/pkill_cluster.sh ${HOSTFILE}-0
+    # sleep 60
 
     
-    horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_imagenet_hvd.py   \
-    --rec-train /home/ubuntu/data/imagenet/train.rec --rec-train-idx /home/ubuntu/data/imagenet/train.idx   \
-    --rec-val /home/ubuntu/data/imagenet/val.rec --rec-val-idx /home/ubuntu/data/imagenet/val.idx   \
-    --model resnet50_v2 --mode hybrid   --lr 0.05 --lr-mode cosine --num-epochs 120 --batch-size 32 -j 6   \
-    --warmup-epochs 5 --use-rec --dtype float16 --optimizer nag \
-    --trainer efsgd --input-sparse-1 1 --output-sparse-1 1 --layer-sparse-1 1024
+    # horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_imagenet_hvd.py   \
+    # --rec-train /home/ubuntu/data/imagenet/train.rec --rec-train-idx /home/ubuntu/data/imagenet/train.idx   \
+    # --rec-val /home/ubuntu/data/imagenet/val.rec --rec-val-idx /home/ubuntu/data/imagenet/val.idx   \
+    # --model resnet50_v2 --mode hybrid   --lr 0.05 --lr-mode cosine --num-epochs 120 --batch-size 32 -j 6   \
+    # --warmup-epochs 5 --use-rec --dtype float16 --optimizer nag \
+    # --trainer efsgd --input-sparse-1 1 --output-sparse-1 1 --layer-sparse-1 1024
 
-    sleep 60
-    bash /home/ubuntu/src/ersgd/ec2-tools/pkill_cluster.sh ${HOSTFILE}-0
-    sleep 60
+    # sleep 60
+    # bash /home/ubuntu/src/ersgd/ec2-tools/pkill_cluster.sh ${HOSTFILE}-0
+    # sleep 60
 
     ############ ersgd
 
--- scripts/classification/imagenet/train_imagenet_hvd.py
+++ scripts/classification/imagenet/train_imagenet_hvd.py
@@ -194,7 +194,7 @@ def main():
     num_batches = num_training_samples // (batch_size * hvd.size())
 
     lr_scheduler = LRSequential([
-        LRScheduler('linear', base_lr=0, target_lr=opt.lr,
+        LRScheduler('linear', base_lr=opt.warmup_lr, target_lr=opt.lr,
                     nepochs=warmup_epochs, iters_per_epoch=num_batches),
         LRScheduler(opt.lr_mode, base_lr=opt.lr, target_lr=0,
                     nepochs=opt.num_epochs - warmup_epochs,
