f7488ceadfd51ddd2f40b0acc1ecc6a2e72a9414
unknown <xcgoner1108@gmail.com>
2020-05-31 22:16:06 -0500

    imagenet

--- scripts/classification/cifar/exp_qsparselocalsgd.sh
+++ scripts/classification/cifar/exp_qsparselocalsgd.sh
@@ -6,6 +6,42 @@ do
     horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_cifar100_hvd_qsparselocalsgd_v1.py \
     --model cifar_wideresnet40_8 --optimizer nag --lr 0.1 --lr-decay 0.2 \
     --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size 16 \
+    --input-sparse 1 --output-sparse 4 --layer-sparse 1 --local-sgd-interval 8
+
+    sleep 60
+    bash /home/ubuntu/src/ersgd/ec2-tools/pkill_cluster.sh ${HOSTFILE}-0
+    sleep 60
+
+    horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_cifar100_hvd_qsparselocalsgd_v1.py \
+    --model cifar_wideresnet40_8 --optimizer nag --lr 0.05 --lr-decay 0.2 \
+    --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size 16 \
+    --input-sparse 32 --output-sparse 4 --layer-sparse 1 --local-sgd-interval 2
+
+    sleep 60
+    bash /home/ubuntu/src/ersgd/ec2-tools/pkill_cluster.sh ${HOSTFILE}-0
+    sleep 60
+
+    horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_cifar100_hvd_qsparselocalsgd_v1.py \
+    --model cifar_wideresnet40_8 --optimizer nag --lr 0.05 --lr-decay 0.2 \
+    --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size 16 \
+    --input-sparse 32 --output-sparse 4 --layer-sparse 1 --local-sgd-interval 8
+
+    sleep 60
+    bash /home/ubuntu/src/ersgd/ec2-tools/pkill_cluster.sh ${HOSTFILE}-0
+    sleep 60
+
+    horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_cifar100_hvd_qsparselocalsgd_v1.py \
+    --model cifar_wideresnet40_8 --optimizer nag --lr 0.05 --lr-decay 0.2 \
+    --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size 16 \
+    --input-sparse 32 --output-sparse 4 --layer-sparse 1 --local-sgd-interval 4
+
+    sleep 60
+    bash /home/ubuntu/src/ersgd/ec2-tools/pkill_cluster.sh ${HOSTFILE}-0
+    sleep 60
+
+    horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_cifar100_hvd_qsparselocalsgd_v1.py \
+    --model cifar_wideresnet40_8 --optimizer nag --lr 0.1 --lr-decay 0.2 \
+    --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size 16 \
     --input-sparse 1 --output-sparse 1 --layer-sparse 1 --local-sgd-interval 2
 
     sleep 60
@@ -40,15 +76,6 @@ do
     sleep 60
 
     horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_cifar100_hvd_qsparselocalsgd_v1.py \
-    --model cifar_wideresnet40_8 --optimizer nag --lr 0.1 --lr-decay 0.2 \
-    --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size 16 \
-    --input-sparse 1 --output-sparse 4 --layer-sparse 1 --local-sgd-interval 8
-
-    sleep 60
-    bash /home/ubuntu/src/ersgd/ec2-tools/pkill_cluster.sh ${HOSTFILE}-0
-    sleep 60
-
-    horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_cifar100_hvd_qsparselocalsgd_v1.py \
     --model cifar_wideresnet40_8 --optimizer nag --lr 0.05 --lr-decay 0.2 \
     --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size 16 \
     --input-sparse 16 --output-sparse 1 --layer-sparse 1 --local-sgd-interval 4
@@ -61,33 +88,6 @@ do
     --model cifar_wideresnet40_8 --optimizer nag --lr 0.05 --lr-decay 0.2 \
     --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size 16 \
     --input-sparse 16 --output-sparse 1 --layer-sparse 1 --local-sgd-interval 8
-
-    sleep 60
-    bash /home/ubuntu/src/ersgd/ec2-tools/pkill_cluster.sh ${HOSTFILE}-0
-    sleep 60
-
-    horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_cifar100_hvd_qsparselocalsgd_v1.py \
-    --model cifar_wideresnet40_8 --optimizer nag --lr 0.05 --lr-decay 0.2 \
-    --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size 16 \
-    --input-sparse 32 --output-sparse 4 --layer-sparse 1 --local-sgd-interval 2
-
-    sleep 60
-    bash /home/ubuntu/src/ersgd/ec2-tools/pkill_cluster.sh ${HOSTFILE}-0
-    sleep 60
-
-    horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_cifar100_hvd_qsparselocalsgd_v1.py \
-    --model cifar_wideresnet40_8 --optimizer nag --lr 0.05 --lr-decay 0.2 \
-    --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size 16 \
-    --input-sparse 32 --output-sparse 4 --layer-sparse 1 --local-sgd-interval 4
-
-    sleep 60
-    bash /home/ubuntu/src/ersgd/ec2-tools/pkill_cluster.sh ${HOSTFILE}-0
-    sleep 60
-
-    horovodrun -np 8 -hostfile ${HOSTFILE} python3 train_cifar100_hvd_qsparselocalsgd_v1.py \
-    --model cifar_wideresnet40_8 --optimizer nag --lr 0.05 --lr-decay 0.2 \
-    --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 200 --batch-size 16 \
-    --input-sparse 32 --output-sparse 4 --layer-sparse 1 --local-sgd-interval 8
 
     sleep 60
     bash /home/ubuntu/src/ersgd/ec2-tools/pkill_cluster.sh ${HOSTFILE}-0
