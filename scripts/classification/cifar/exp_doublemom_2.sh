ea9c7a10801f3e2667a3ee012bd32366b86e4a53
unknown <xcgoner1108@gmail.com>
2020-06-20 23:07:44 -0500

    doublemom

--- scripts/classification/cifar/exp_doublemom_2.sh
+++ scripts/classification/cifar/exp_doublemom_2.sh
@@ -5,7 +5,7 @@ for repeat in 1; do
         for localm in 0.1 0.2 0.5; do
             horovodrun -np 8 -H localhost:8 python3 train_cifar_hvd_doublemom.py \
             --model cifar_wideresnet40_8 --local-optimizer nag --global-optimizer nag \
-            --local-lr 0.1 --global-lr 0.1 --lr-decay 0.5 --lr-decay-epoch 30,60,80 \
+            --local-lr 0.1 --global-lr 0.09 --lr-decay 0.2 --lr-decay-epoch 30,60,80 \
             --local-momentum ${localm} --global-momentum 0.9 \
             --wd 0.0005 --num-epochs 100 --batch-size 16 --local-sgd-interval ${H} \
             --mode hybrid -j 4 --nclasses 100
