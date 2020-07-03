ee7dec7cf683641bc21d2b8234ba1850a51355be
unknown <xcgoner1108@gmail.com>
2020-07-03 18:36:06 -0500

    test throughput

--- scripts/classification/cifar/exp_ersgd_throughput.sh
+++ scripts/classification/cifar/exp_ersgd_throughput.sh
@@ -13,7 +13,7 @@ do
                 horovodrun -np ${nworkers} -H localhost:${nworkers} python3 train_cifar100_hvd_ersgd_v2.py \
                 --model cifar_wideresnet40_8 --optimizer nag --lr ${lr} --lr-decay 0.2 \
                 --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 15 --batch-size ${batchsize} \
-                --input-sparse 1 --output-sparse 1 --layer-sparse ${layersparse} --warmup 2
+                --input-sparse 1 --output-sparse 1 --layer-sparse ${layersparse} --warmup 2 --test-throughput
 
                 sleep 60
                 pkill -9 python3
--- scripts/classification/cifar/exp_sgd_throughput.sh
+++ scripts/classification/cifar/exp_sgd_throughput.sh
@@ -8,7 +8,7 @@ do
         do
             horovodrun -np ${nworkers} -H localhost:${nworkers} python3 train_cifar100_hvd.py \
             --model cifar_wideresnet40_8 --optimizer nag --lr ${lr} --lr-decay 0.2 \
-            --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 15 --batch-size ${batchsize}
+            --lr-decay-epoch 60,120,160 --wd 0.0005 --num-epochs 15 --batch-size ${batchsize}  --test-throughput
 
             sleep 60
             pkill -9 python3
